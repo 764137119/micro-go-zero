9dc939a4f62f2adb43576cd8e1936dfb1962b378
wangyingwen <wangyingwen@wangyingwendeMacBook-Pro.local>
2026-06-16 12:15:28 +0800

    服务注册加ectd 配置

--- user-service/internal/svc/servicecontext.go
+++ user-service/internal/svc/servicecontext.go
@@ -4,19 +4,24 @@
 package svc
 
 import (
-	"github.com/zeromicro/go-zero/rest"
+	"user-rpc/userclient"
 	"user-service/internal/config"
 	"user-service/internal/middleware"
+
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
 	Config         config.Config
 	AuthMiddleware rest.Middleware
+	UserRpc        userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:         c,
 		AuthMiddleware: middleware.NewAuthMiddleware().Handle,
+		UserRpc:        userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
