1ff9450d3311460de1e324d0b3e7b49979343836
764137119 <764137119@qq.com>
2026-06-22 14:09:13 +0800

    fix-config 加json 标签

--- api-gateway/config/config.go
+++ api-gateway/config/config.go
@@ -3,21 +3,21 @@ package config
 import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
-	Name string `yaml:"Name"`
-	Host string `yaml:"Host"`
-	Port int    `yaml:"Port"`
+	Name string `json:"Name" yaml:"Name"`
+	Host string `json:"Host" yaml:"Host"`
+	Port int    `json:"Port" yaml:"Port"`
 
-	UserRpc    zrpc.RpcClientConf `yaml:"UserRpc"`
-	OrderRpc   zrpc.RpcClientConf `yaml:"OrderRpc"`
-	StockRpc   zrpc.RpcClientConf `yaml:"StockRpc"`
-	CronJobRpc zrpc.RpcClientConf `yaml:"CronJobRpc"`
+	UserRpc    zrpc.RpcClientConf `json:"UserRpc" yaml:"UserRpc"`
+	OrderRpc   zrpc.RpcClientConf `json:"OrderRpc" yaml:"OrderRpc"`
+	StockRpc   zrpc.RpcClientConf `json:"StockRpc" yaml:"StockRpc"`
+	CronJobRpc zrpc.RpcClientConf `json:"CronJobRpc" yaml:"CronJobRpc"`
 
 	// DTM HTTP 地址（用于生成 gid），如 "http://dtm:36789"
-	DTM string `yaml:"DTM"`
+	DTM string `json:"DTM" yaml:"DTM"`
 	// DTM gRPC 地址（用于提交 Saga），如 "dtm:36790"
-	DTMEndpoint string `yaml:"DTMEndpoint"`
+	DTMEndpoint string `json:"DTMEndpoint" yaml:"DTMEndpoint"`
 
 	// Saga 分支的 gRPC 目标地址（供 dtm 回调调用）
-	StockRpcTarget string `yaml:"StockRpcTarget"` // "stock-rpc:8082"
-	OrderRpcTarget string `yaml:"OrderRpcTarget"` // "order-rpc:8081"
+	StockRpcTarget string `json:"StockRpcTarget" yaml:"StockRpcTarget"` // "stock-rpc:8082"
+	OrderRpcTarget string `json:"OrderRpcTarget" yaml:"OrderRpcTarget"` // "order-rpc:8081"
 }
